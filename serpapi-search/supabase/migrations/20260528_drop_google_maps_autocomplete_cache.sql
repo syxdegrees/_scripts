7f09e55e7337e5cceef2f742cddf618223b90a88
syxdegrees <jesh@syxdegrees.com>
2026-05-28 18:25:20 -0500

    chore: remove all serpapi script directories

--- serpapi-search/supabase/migrations/20260528_drop_google_maps_autocomplete_cache.sql
+++
@@ -1,3 +0,0 @@
--- google_maps_autocomplete requires ll (GPS coords) as a required param,
--- making it impractical for general search use. Engine removed from skill.
-DROP TABLE IF EXISTS serpapi_google_maps_autocomplete_cache;
